967897c9343ef65f4710a77b5ba949bba8512f50
yasedrisian <yasedrisian-KnNIvq>
2023-04-05 11:07:25 -0400

    completing with defining adding removing and show function

--- server.js
+++ server.js
@@ -76,3 +76,101 @@ const firstprompt = () => {
       let selectedEmployee;
       let selectedRole;
       let selectedDepartment;
+
+
+      switch (menu) {
+        case "View all departments":
+          const departments = await department.getAllDepartmentNames();
+          if (departments) console.table(departments);
+          else console.log("There isn't any department");
+          break;
+    
+        case "View all roles":
+          const selectedRoleTitle = await role.showSelectroleMenu();
+    
+          if (!selectedRoleTitle) {
+            console.log("There isn't any role.");
+            break;
+          }
+    
+          selectedRole = await role.getByTitle(selectedRoleTitle);
+          const roleDepartment = await department.get(selectedRole.department_id);
+    
+          let departmentName = null;
+          if (roleDepartment) {
+            departmentName = roleDepartment.name;
+          }
+    
+          console.table({
+            title: selectedRole.title,
+            salary: selectedRole.salary,
+            department: departmentName,
+          });
+          break;
+    
+        case "View all employees":
+          const employeeName = await employee.showSelectEmployeeMenu(
+            "Select an employee to view its informations."
+          );
+    
+          if (!employeeName) {
+            console.log("There isn't any employee");
+            break;
+          }
+    
+          [firstName, lastName] = employeeName.split(" ");
+          selectedEmployee = await employee.getByName(firstName, lastName);
+          const manager = await employee.get(selectedEmployee.manager_id);
+          let managerName = null;
+          if (manager) {
+            managerName = manager.first_name + " " + manager.last_name;
+          }
+    
+          const employeeRole = await role.get(selectedEmployee.role_id);
+          let employeeRoleTitle = null;
+          if (employeeRole) {
+            employeeRoleTitle = employeeRole.title;
+          }
+          console.table({
+            "first name": selectedEmployee.first_name,
+            "last name": selectedEmployee.last_name,
+            manager: managerName,
+            role: employeeRoleTitle,
+          });
+          break;
+    
+        case "Add a department":
+          inputs = await department.showAddMenu();
+          const newDepartment = {
+            name: inputs["departmentName"],
+          };
+    
+          const newdepartmentId = await department.add(newDepartment);
+          console.log(`Department with id=${newdepartmentId} inserted`);
+          break;
+          case "Add an employee":
+            inputs = await employee.showAddMenu();
+      
+            managerId = null;
+            if (inputs["manager"]) {
+              const managerName = inputs["manager"].split(" ");
+              managerId = (await employee.getByName(managerName[0], managerName[1]))
+                .id;
+            }
+      
+            selectedRole = await role.getByTitle(inputs["roleTitle"]);
+            roleId = null;
+            if (selectedRole) {
+              roleId = selectedRole.id;
+            }
+      
+            const newEmployee = {};
+            newEmployee["first_name"] = inputs["first"];
+            newEmployee["last_name"] = inputs["last"];
+            newEmployee["role_id"] = roleId;
+            newEmployee["manager_id"] = managerId;
+      
+            const newEmployeeId = await employee.add(newEmployee);
+            console.log(`Employee with id=${newEmployeeId} inserted`);
+            break;
+          
